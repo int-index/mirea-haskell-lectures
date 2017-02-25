af7644925baf50137d85ae746d37727fe47bec31
Sergey Chooh <chooh@chooh.msk.ru>
2017-02-25 11:32:48 +0300

    Функция foldr

---
+++ chooh/Challenge.hs
@@ -0,0 +1,41 @@
+{-# LANGUAGE TemplateHaskell, UnicodeSyntax #-}
+
+module Challenge where
+
+import Prelude()
+import Magic
+
+data Nat = Succ Nat | Zero
+magicNat ''Nat
+
+data Bool = False | True
+  deriving (Show)
+
+data Maybe α = Nothing | Just α
+  deriving (Show)
+
+{- Helpers -}
+
+(.) :: (β -> γ) -> (α -> β) -> (α -> γ)
+(.) f g x = f (g x)
+
+(+), (*) :: Nat -> Nat -> Nat
+
+x + y =
+  case x of
+    Zero   -> y
+    Succ a -> Succ (a + y)
+
+x * y =
+  case x of
+    Zero   -> Zero
+    Succ a -> y + (a * y)
+
+foldr :: (α -> β -> β) ->
+          β ->
+          [α] ->
+          β
+foldr f acc list =
+  case list of
+    [] -> acc
+    x : xs -> foldr f (f x acc) xs
