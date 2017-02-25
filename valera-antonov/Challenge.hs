ae2cb9d919105ad76c183ed1c1ccb91779039b7d
aulandsdalen <aulandsdalen@gmail.com>
2017-02-25 11:51:31 +0300

    Challenge: foldr

---
+++ valera-antonov/Challenge.hs
@@ -0,0 +1,38 @@
+{-# LANGUAGE TemplateHaskell, NoImplicitPrelude #-}
+
+module Challenge where
+import Prelude()
+
+import Magic 
+
+data Bool = False | True 
+    deriving (Show)
+
+data Nat = Succ Nat | Zero 
+magicNat ''Nat
+
+-- data List element = Nil | Cons element (List element)
+-- data [element] = [] | (:) element [element]
+
+(+), (*) :: Nat -> Nat -> Nat
+
+x + y =
+  case x of
+    Zero   -> y
+    Succ a -> Succ (a + y)
+
+x * y =
+  case x of
+    Zero   -> Zero
+    Succ a -> y + (a * y)
+
+
+foldr :: (element -> accum -> accum) ->
+          accum ->
+          [element] ->
+          accum 
+
+foldr cons nil xs = 
+    case xs of
+        [] -> nil
+        (x:xs') -> cons x (foldr cons nil xs')
