2e48ff7693972b9cacb87f75c5aacd97513a374d
dartaan <vbif.32@ya.ru>
2017-02-25 13:53:37 +0300

    length

--- mikhail-fedin/challenge.hs
+++ mikhail-fedin/challenge.hs
@@ -47,6 +47,12 @@ map func list =
 -- map func list = foldr (\element list' -> func element : list') [] list
 -- map func = foldr ((:) . func) []
 
+const :: a -> b -> a
+const x _ = x
+
+id :: a -> a
+id x = x
+
 flip :: (a -> b -> c) -> b -> a -> c
 flip f x y = f y x
 
@@ -54,5 +60,45 @@ flip f x y = f y x
 --(++) list1 list2 = foldr (:) list2 list1
 (++) = flip (foldr (:))
 
+sum :: [Nat] -> Nat
+sum = foldr (+) 0
+
+product :: [Nat] -> Nat
+product = foldr (*) 1
+
 concat :: [[element]] -> [element]
 concat = foldr (++) []
+
+not :: Bool -> Bool
+not x =
+  case x of
+    True -> False
+    False -> True
+
+odd, even :: Nat -> Bool
+even x =
+  case x of
+    Zero -> True
+    (Succ a) -> odd a
+odd x = not (even x)
+
+filter :: (element -> Bool) -> [element] -> [element]
+filter func list =
+  case list of
+    [] -> []
+    (element' : list') ->
+     case func element' of
+      False -> filter func list'
+      True ->  element' : (filter func list')
+--filter func list = foldr
+-- (\x xs -> case p x of
+--    True -> x : xs
+--    False -> xs)
+--  []
+
+--ifThenElse :: Bool -> a -> a ->a
+--ifThenElse cond
+--filter func = foldr (\element -> ifThenElse (func element) (element :) id) []
+
+length :: [element] -> Nat
+length = foldr (const Succ) Zero
